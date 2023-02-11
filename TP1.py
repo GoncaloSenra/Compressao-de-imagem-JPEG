5861069d2f6ebc0fae61b67e5e0789bd13ca0eaa
GoncaloSenra <goncalosenra@gmail.com>
2023-02-11 14:30:36 +0000

    Add files

---
+++ TP1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def main():
+    p = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    p.shape = (2, 5)
+    print(p)
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()
