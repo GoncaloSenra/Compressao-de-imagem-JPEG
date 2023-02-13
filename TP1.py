00392045147a1bde254aa6774a5251e35b0bee96
GoncaloSenra <goncalosenra@gmail.com>
2023-02-13 14:31:04 +0000

    imagens

--- TP1.py
+++ TP1.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-
-def main():
-    p = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    p.shape = (2, 5)
-    print(p)
+def encoder():
     pass
 
+def decoder():
+    pass
 
-
-
+def main():
+    pass
 
 if __name__ == '__main__':
     main()
