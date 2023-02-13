f80e763534829360405f7f4ed54c7ea72e4c49ec
JoaoBCoelh0 <114809158+JoaoBCoelh0@users.noreply.github.com>
2023-02-13 15:35:29 +0000

    Pila

--- TP1.py
+++ TP1.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.colors as clr
+
 
 def encoder():
     pass
@@ -6,8 +9,62 @@ def encoder():
 def decoder():
     pass
 
+def colorMapEnc(img):
+    
+    #Encoder
+    cmRed = clr.LinearSegmentedColormap.from_list('red', [(0,0,0), (1,0,0)], 256)
+    cmGreen = clr.LinearSegmentedColormap.from_list('green', [(0,0,0), (0,1,0)], 256)
+    cmBlue = clr.LinearSegmentedColormap.from_list('blue', [(0,0,0), (0,0,1)], 256)
+
+    R = img[:,:,0]
+    G = img[:,:,1]
+    B = img[:,:,2]
+    
+    showImageColormap(R, cmRed)
+    showImageColormap(G, cmGreen)
+    showImageColormap(B, cmBlue)
+    
+    
+    '''
+    N funfa
+    #Decoder
+    [nl, nc, ch] = img.shape
+    imgRec = np.zeros((nl, nc, ch))
+    imgRec[:,:,0] = R
+    imgRec[:,:,1] = G
+    imgRec[:,:,2] = B
+    showImage(imgRec)
+    '''
+    
+    
+def showImageColormap(auxColormap1,auxColormap2):
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(auxColormap1, auxColormap2)
+    
+    
+def showImage(img):
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(img, cmap = "gray")
+    #print(img.shape)
+    
+    
 def main():
-    pass
+    
+    #Ler imagens
+    img1 = plt.imread('imagens/barn_mountains.bmp')
+    img2 = plt.imread('imagens/logo.bmp')    
+    img3 = plt.imread('imagens/peppers.bmp')
+
+    #Mostrar imagens
+    showImage(img1)
+    colorMapEnc(img1)
+    #showImage(img2)
+    #showImage(img3)
+    
+    
+    
 
 if __name__ == '__main__':
     main()
