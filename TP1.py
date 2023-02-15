2ed31d224ad1f8aad14b677d182db93512aa34d9
GoncaloSenra <goncalosenra@gmail.com>
2023-02-15 14:58:23 +0000

    ycbcr

--- TP1.py
+++ TP1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.colors as clr
-
+from PIL import Image
 
 def encoder():
     pass
@@ -75,6 +75,62 @@ def padding_inv(pad, shape):
     
     ipad = pad[:-nl,:-nc]
     showImage(ipad)
+
+def convert_ycbcr(img):
+    mat = np.array([[0.299, 0.587, 0.114], [-0.168736, -0.331264, 0.5], [0.5, -0.418688, -0.081312]])
+
+    y = np.dot(img, mat.T)
+    #y = np.zeros_like(img)
+    #y = mat[0,0] * img[:,:,0] + mat[0,1] * img[:,:,1] + mat[0,2]  * img[:,:,2]
+    
+    y[:,:,0] = y[:,:,0] + 0
+    y[:,:,1] = y[:,:,1] + 128
+    y[:,:,2] = y[:,:,2] + 128
+    
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y.astype(np.uint8))
+    plt.axis("off")
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,0].astype(np.uint8), cmap="gray")
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,1].astype(np.uint8))
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,2].astype(np.uint8))
+
+    return y
+
+def convert_rgb(img):
+    mat = np.array([[0.299, 0.587, 0.114], [-0.168736, -0.331264, 0.5], [0.5, -0.418688, -0.081312]])
+
+    matI = np.linalg.inv(mat)
+    print(matI)
+    
+    y = np.dot(img, matI.T)
+    y[:,:,0] = y[:,:,0] - 0
+    y[:,:,1] = y[:,:,1] - 128
+    y[:,:,2] = y[:,:,2] - 128
+
+    y[y>255] = 255
+    y[y<0] = 0
+    
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y.astype(np.uint8))
+    plt.axis("off")
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,0].astype(np.uint8))
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,1].astype(np.uint8))
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(y[:,:,2].astype(np.uint8))
+
     
     
 
@@ -104,9 +160,11 @@ def main():
     #showImage(img2)
     #showImage(img3)
     #plt.close('all')
-    pad = padding(img1)
-    padding_inv(pad, img1.shape)
-    
+    #pad = padding(img1)
+    #padding_inv(pad, img1.shape)
+    ycbcr = convert_ycbcr(img1)
+    convert_rgb(ycbcr)
+
 
 if __name__ == '__main__':
     main()
