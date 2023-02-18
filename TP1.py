432ecf10b58ac8b7c8b481c61377fc7eb552c4cb
Rikes8 <92479136+Rikes8@users.noreply.github.com>
2023-02-18 00:57:19 +0000

    Update TP1.py
    Tudo a implementado na funcao decoder e encoder  + alteracoes sugeridas pelo stor (nomes, ciclos, ...)

--- TP1.py
+++ TP1.py
@@ -8,40 +8,41 @@ import matplotlib.colors as clr
  4:2:0 Ambos os canais sao comprimmidos de igual forma (2:1), mas na Horizontal e na Vertical
  import cv2
  cv2.resize(matriz, fatores de compressao, tipo de interpolacao (linear, cubica, etc))
-
                        (4:2:2)              (4:2:0)
        300x300         150(H):300(V)        150(H):150(V)
        
-
 no decoder: fazer o contrario se foi reduzido a metade passar para o dobro (operaçao destrutiva)
-
-
 #DCT (7)
 usar dct sobre os canais que queremos
-
 """
 
 
 def encoder(img):
-    
+    #print(img.shape)
     #3) Color maps RGB
     R, G, B , cmRed, cmGreen, cmBlue = colorMapEnc(img)
     
         
     #4) Padding
-    padding(img)
+    padding_img , l ,c= padding(img)
     padding(R, cmRed)
     padding(G, cmGreen)
     padding(B, cmBlue)
     
 
     #5) Convert to YCbCr
-    ycbcr = convert_ycbcr(img)
-
+    ycbcr = convert_ycbcr(padding_img)
+    
+    
+    return ycbcr, l, c 
     
 
-def decoder(img, shape):
-    convert_rgb(img)
+def decoder(img, line, col):
+    
+    imgRGB  = convert_rgb(img)
+    
+    padding_inv(imgRGB, line, col)
+
 
 def colorMapEnc(img):
     
@@ -71,21 +72,17 @@ def colorMapEnc(img):
     """
     return R, G, B, cmRed, cmGreen, cmBlue
     
+
 def padding(img, colormap = None):
 
     xp = img
     shape = img.shape
-    c = shape[1]
-    l = shape[0]
+    col = shape[1]
+    line = shape[0]
 
-    while c % 32 != 0:
-        c += 1
-        
-    while l % 32 != 0:
-        l += 1
 
-    nl = l - shape[0]
-    nc = c - shape[1]
+    nl = 32 - line % 32
+    nc = 32 - col % 32
     
     ll = img[shape[0] - 1, :] [np.newaxis, :]
     
@@ -104,15 +101,17 @@ def padding(img, colormap = None):
     
     #print(xt.shape)
     
-    return xt
+    return xt, line, col
 
 
-def padding_inv(pad, shape):
-    nl = pad.shape[0] - shape[0]
-    nc = pad.shape[1] - shape[1]
+
+def padding_inv(padding_img, line, col):
+    nl = padding_img.shape[0] - line
+    nc = padding_img.shape[1] - col
     
-    ipad = pad[:-nl,:-nc]
-    showImageColormap(ipad, "Reversed padding")
+    un_padding = padding_img[:-nl,:-nc]
+    showImageColormap(un_padding, "Reversed padding")
+    #print(ipad.shape)
 
 def convert_ycbcr(img):
     mat = np.array([[0.299, 0.587, 0.114], [-0.168736, -0.331264, 0.5], [0.5, -0.418688, -0.081312]])
@@ -126,7 +125,7 @@ def convert_ycbcr(img):
     y[:,:,2] = y[:,:,2] + 128
     
 
-    showImageColormap(y, "YCbCr")
+    #showImageColormap(y, "YCbCr")
     
     showImageColormap(y[:,:,0], "Y", "gray")
     
@@ -145,15 +144,17 @@ def convert_rgb(img):
     img[:,:,1] = img[:,:,1] - 128
     img[:,:,2] = img[:,:,2] - 128
 
-    y = np.dot(img, matI.T)
+    rgb_img = np.dot(img, matI.T)
     
 
-    y[y>255] = 255
-    y[y<0] = 0
+    rgb_img[rgb_img>255] = 255
+    rgb_img[rgb_img<0] = 0
     
-    y = np.round(y).astype(np.uint8)
+    rgb_img = np.round(rgb_img).astype(np.uint8)
     
-    showImageColormap(y, "RGB")
+    showImageColormap(rgb_img, "RGB")
+    
+    return rgb_img
  
     
 #3.3) 
@@ -176,8 +177,8 @@ def main():
     #Escolher imagem
     imgx = img1
 
-    encoder(imgx)
-    #decoder(imgx, imgx.shape)
+    out, line, col = encoder(imgx)
+    decoder(out , line, col)
 
 if __name__ == '__main__':
     main()
